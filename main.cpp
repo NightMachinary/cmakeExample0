7d8b0d79da187e7ec2fcaa69ca23e7415b2add87
NightMachinary <rudiwillalwaysloveyou@gmail.com>
2020-08-02 15:37:27 +0430

    micro changes

--- main.cpp
+++ main.cpp
@@ -2,7 +2,7 @@
 #include <stdio.h>
 #include <curl/curl.h>
 #include <string>
-
+#include <cpr/cpr.h>
 
 static size_t WriteCallback(void *contents, size_t size, size_t nmemb, void *userp)
 {
@@ -19,7 +19,7 @@ int main() {
 
     curl = curl_easy_init();
     if(curl) {
-        curl_easy_setopt(curl, CURLOPT_URL, "http://www.google.com");
+        curl_easy_setopt(curl, CURLOPT_URL, "https://www.google.com");
         curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, WriteCallback);
         curl_easy_setopt(curl, CURLOPT_WRITEDATA, &readBuffer);
         res = curl_easy_perform(curl);
@@ -28,8 +28,20 @@ int main() {
         std::cout << readBuffer << std::endl;
 
         std::cout << "res: " << res << std::endl ;
+
+        cpr::Response r = cpr::Get(cpr::Url{"https://ipinfo.io"}
+//                                   ,cpr::Authentication{"user", "pass"},
+//                                   cpr::Parameters{{"anon", "true"}, {"key", "value"}}
+                                   );
+
+//        cpr::Response r = cpr::Get(cpr::Url{"http://www.httpbin.org/get"});
+
+        std::cout << "status: " << r.status_code << std::endl;                  // 200
+
+        std::cout << "content-type: " << r.header["content-type"] << std::endl;       // application/json; charset=utf-8
+        std::cout << "text: " << r.text  << std::endl          ;               // JSON text string
+
     }
     return 0;
 
-    return 0;
 }
